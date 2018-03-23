66d2b2420caf99b301665556ea7a19d137c1a426
James Lucktaylor <jlucktay@gmail.com>
2018-03-23 16:42:04 +0000

    https://golang.org/ref/spec#The_zero_value

---
+++ the-zero-value/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// T is an example struct to show off the zero value behaviour
+type T struct {
+	i    int
+	f    float64
+	next *T
+}
+
+func main() {
+	t1 := new(T)
+	fmt.Printf("%t\n", t1.i == 0)
+	fmt.Printf("%t\n", t1.f == 0.0)
+	fmt.Printf("%t\n", t1.next == nil)
+
+	var t2 T
+	fmt.Printf("%t\n", t2.i == 0)
+	fmt.Printf("%t\n", t2.f == 0.0)
+	fmt.Printf("%t\n", t2.next == nil)
+}
