cafc1e103ac68adbaaddaf9422543e84913909bb
James Lucktaylor <jlucktay@gmail.com>
2018-03-23 16:42:20 +0000

    Concurrent prime sieve

---
+++ prime-sieve/me/primes.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+
+	"golang.org/x/text/message"
+)
+
+func main() {
+	p := message.NewPrinter(message.MatchLanguage("en"))
+
+	for x := range sieve(gen(1e3)) {
+		p.Printf("%24d\n", x)
+	}
+}
+
+func sieve(in <-chan int64) <-chan int64 {
+	fmt.Println("sieve/outer: start")
+
+	out := make(chan int64)
+
+	go func() {
+		fmt.Println("sieve/inner: start")
+
+		for x := range in {
+			i := big.NewInt(x)
+
+			if i.ProbablyPrime(10) {
+				out <- i.Int64()
+			}
+		}
+
+		close(out)
+
+		fmt.Println("sieve/inner: finish")
+	}()
+
+	fmt.Println("sieve/outer: finish")
+
+	return out
+}
+
+func gen(limit int64) <-chan int64 {
+	fmt.Println("gen/outer: start")
+
+	out := make(chan int64)
+
+	go func() {
+		fmt.Println("gen/inner: start")
+
+		for index := int64(0); index < limit; index++ {
+			out <- index
+		}
+
+		close(out)
+
+		fmt.Println("gen/inner: finish")
+	}()
+
+	fmt.Println("gen/outer: finish")
+
+	return out
+}
