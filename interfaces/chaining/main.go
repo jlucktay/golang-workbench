d14b243b2638501b5424bfdd7e6b921512fa8d17
James Lucktaylor <jlucktay@gmail.com>
2018-03-23 16:45:09 +0000

    Example of chaining interfaces from a good YouTube presentation

---
+++ interfaces/chaining/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"compress/gzip"
+	"encoding/base64"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	const input = `H4sIAAAAAAAA/3qyd8GT3WueLt37fk/Ps46JT/d1vFw942nrlqezFzzZsQskMmfFi/0zX7b3cAECAAD//0G6Zq8rAAAA`
+	var r io.Reader = strings.NewReader(input)
+	r = base64.NewDecoder(base64.StdEncoding, r)
+	r, err := gzip.NewReader(r)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.Copy(os.Stdout, r)
+}
