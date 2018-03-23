3c577867904e9eff654f14c57c8d1c6d8f9f76cd
James Lucktaylor <jlucktay@gmail.com>
2018-03-23 16:43:59 +0000

    Marshalling JSON into a custom struct

---
+++ encoding/json/marshal/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type colorGroup struct {
+	ID     int
+	Name   string
+	Colors []string
+}
+
+func main() {
+	group := colorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+
+	b, err := json.Marshal(group)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+}
