f588c9ece5ba31fb356d76620db1c9854ae941bf
James Lucktaylor <jlucktay@gmail.com>
2018-03-29 14:17:15 +0100

    Following an article and getting this example up and running

---
+++ go_rest_api/pkg/mongo/user_service.go
@@ -0,0 +1,28 @@
+package mongo
+
+import (
+	"../../../go_rest_api/pkg/root"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserService struct {
+	collection *mgo.Collection
+}
+
+func NewUserService(session *Session, dbName string, collectionName string) *UserService {
+	collection := session.GetCollection(dbName, collectionName)
+	collection.EnsureIndex(userModelIndex())
+	return &UserService{collection}
+}
+
+func (p *UserService) Create(u *root.User) error {
+	user := newUserModel(u)
+	return p.collection.Insert(&user)
+}
+
+func (p *UserService) GetByUsername(username string) (*root.User, error) {
+	model := userModel{}
+	err := p.collection.Find(bson.M{"username": username}).One(&model)
+	return model.toRootUser(), err
+}
