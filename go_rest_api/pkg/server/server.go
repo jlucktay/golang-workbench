2abe857a7595c94a727117b3590dbf4c856dfed2
James Lucktaylor <jlucktay@gmail.com>
2018-03-29 14:52:03 +0100

    Part 3 has errors

---
+++ go_rest_api/pkg/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+
+	"github.com/jlucktay/golang-workbench/go_rest_api/pkg"
+)
+
+type Server struct {
+	router *mux.Router
+}
+
+func NewServer(u root.UserService) *Server {
+	s := Server{router: mux.NewRouter()}
+	NewUserRouter(u, s.newSubrouter("/user"))
+	return &s
+}
+
+func (s *Server) Start() {
+	log.Println("Listening on port 8080")
+	if err := http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, s.router)); err != nil {
+		log.Fatal("http.ListenAndServe: ", err)
+	}
+}
+
+func (s *Server) newSubrouter(path string) *mux.Router {
+	return s.router.PathPrefix(path).Subrouter()
+}
