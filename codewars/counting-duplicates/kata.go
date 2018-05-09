dfba574d3404b7d368c9c1cc34d875face9d45c6
James Lucktaylor <jlucktay+github@gmail.com>
2018-05-09 21:08:56 +0100

    Optimised

--- codewars/counting-duplicates/kata.go
+++ codewars/counting-duplicates/kata.go
@@ -1,25 +1,19 @@
 package kata
 
 import (
-	"unicode"
+	"strings"
 )
 
 // Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string.
 // The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
-func duplicateCount(s1 string) int {
+func duplicateCount(s string) (result int) {
 	parse := map[rune]uint{}
 
-	for _, r := range []rune(s1) {
-		parse[unicode.ToLower(r)]++
-	}
-
-	result := 0
-
-	for _, x := range parse {
-		if x > 1 {
+	for _, r := range strings.ToLower(s) {
+		if parse[r]++; parse[r] == 2 {
 			result++
 		}
 	}
 
-	return result
+	return
 }
