20d0180e57eaa7591c76f53995110edf2a534702
James Lucktaylor <jlucktay+github@gmail.com>
2018-07-09 09:33:01 +0100

    Comments

--- os/go-env/main.go
+++ os/go-env/main.go
@@ -11,7 +11,7 @@ func main() {
 	for _, e := range os.Environ() {
 		v := strings.Split(e, "=")
 
-		// Variables containing 'termcap' have some annoying colour codes, so we filter them out
+		// Variables containing 'termcap' have some annoying colour codes, so we don't print those
 		if !strings.Contains(strings.ToLower(v[0]), "termcap") {
 			fmt.Printf("'%s' = '%s'\n", v[0], v[1:])
 		}
