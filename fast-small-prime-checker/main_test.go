58058ab3a0efaefc49857c6b7e9b5f4093596898
James Lucktaylor <jlucktay+github@gmail.com>
2018-07-14 14:28:50 +0100

    Added another prime checker func (albeit for uint64) from elsewhere

--- fast-small-prime-checker/main_test.go
+++ fast-small-prime-checker/main_test.go
@@ -26,6 +26,16 @@ func i32Prime(n uint32) bool {
 	return true
 }
 
+func isPrime(value uint64) bool {
+	for i := uint64(2); i <= uint64(math.Floor(float64(value)/2)); i++ {
+		if value%i == 0 {
+			return false
+		}
+	}
+
+	return value > 1
+}
+
 //const num = 65533051
 const num = 1<<19 - 1
 
@@ -45,6 +55,13 @@ func TestPPrime(t *testing.T) {
 	}
 }
 
+func TestIPrime(t *testing.T) {
+	prime := uint64(num)
+	if !isPrime(prime) {
+		t.Error(prime, "failed prime test")
+	}
+}
+
 func BenchmarkPrime(b *testing.B) {
 	prime := uint32(num)
 	for n := 0; n < b.N; n++ {
@@ -56,5 +73,12 @@ func BenchmarkPPrime(b *testing.B) {
 	prime := big.NewInt(num)
 	for n := 0; n < b.N; n++ {
 		prime.ProbablyPrime(0)
+	}
+}
+
+func BenchmarkIPrime(b *testing.B) {
+	prime := uint64(num)
+	for n := 0; n < b.N; n++ {
+		isPrime(prime)
 	}
 }
