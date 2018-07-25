4817c8767e213097d6c4f3ebd7bcbdbc2d2ec7ea
James Lucktaylor <jlucktay+github@gmail.com>
2018-07-25 17:36:08 +0100

    Added JWT example of token creation with custom claims

---
+++ jwt/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	mySigningKey = "WOW,MuchShibe,ToDogge"
+)
+
+func main() {
+	createdToken, err := exampleNew([]byte(mySigningKey))
+	if err != nil {
+		fmt.Println("Creating token failed")
+	}
+	exampleParse(createdToken, mySigningKey)
+}
+
+func exampleNew(mySigningKey []byte) (string, error) {
+	// Create the token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": "bar",
+		"exp": time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string
+	tokenString, err := token.SignedString(mySigningKey)
+	return tokenString, err
+}
+
+func exampleParse(myToken string, myKey string) {
+	token, err := jwt.Parse(myToken, func(token *jwt.Token) (interface{}, error) {
+		return []byte(myKey), nil
+	})
+
+	if err == nil && token.Valid {
+		fmt.Println("Your token is valid.  I like your style.")
+		fmt.Printf("%+v\n", token)
+	} else {
+		fmt.Println("This token is terrible!  I cannot accept this.")
+	}
+}
