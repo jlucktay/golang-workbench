8d12f13b9c6c765f1c1a66c6d1e5069c7f2984d1
James Lucktaylor <jlucktay+github@gmail.com>
2018-08-20 21:05:43 +0100

    How To Max Out Your CPU And Influence People

---
+++ cpu-max/main.go
@@ -0,0 +1,27 @@
+// From here:
+// https://stackoverflow.com/questions/41079492/golang-code-to-increase-cpu-usage
+package main
+
+import (
+	"runtime"
+	"time"
+)
+
+func main() {
+	done := make(chan int)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				default:
+				}
+			}
+		}()
+	}
+
+	time.Sleep(time.Minute * 5)
+	close(done)
+}
