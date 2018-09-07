96272a855285068fcb74098c8c58f32d8eac8ea4
James Lucktaylor <jlucktay+github@gmail.com>
2018-09-07 10:21:14 +0100

    Making use of new tiny 'secrets' (sub-)package

--- github/dotfiles/secrets.go
+++ github/dotfiles/secrets.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-)
+import "github.com/jlucktay/golang-workbench/secrets"
 
 var (
-	ghpaToken = readTokenFromSecrets("./secrets.json")
+	ghpaToken = secrets.ReadTokenFromSecrets("./secrets.json")
 )
-
-type secrets struct {
-	GitHubPersonalAccessToken string
-}
-
-func readTokenFromSecrets(path string) (token string) {
-	fileContents, readErr := ioutil.ReadFile(path)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	var tokenMap map[string]string
-
-	if umErr := json.Unmarshal(fileContents, &tokenMap); umErr != nil {
-		log.Fatal(umErr)
-	}
-
-	if t, ok := tokenMap["GitHubPersonalAccessToken"]; ok {
-		token = t
-	}
-
-	return
-}
