482657b4ec254f1bc5e1639ee3b5c597d2a4dac5
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-22 12:50:40 +0100

    Start https://www.hackerrank.com/challenges/ctci-array-left-rotation

---
+++ hackerrank/ctci-array-left-rotation/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestRotLeft(t *testing.T) {
+	testCases := []struct {
+		a    []int32
+		d    int32
+		want []int32
+	}{
+		{
+			a:    []int32{1, 2, 3, 4, 5},
+			d:    4,
+			want: []int32{5, 1, 2, 3, 4},
+		},
+		{
+			a:    []int32{41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51},
+			d:    10,
+			want: []int32{77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77},
+		},
+		{
+			a:    []int32{33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97},
+			d:    13,
+			want: []int32{87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(fmt.Sprintf("%v <- %v", tC.a, tC.d), func(t *testing.T) {
+			if result := rotLeft(tC.a, tC.d); !reflect.DeepEqual(result, tC.want) {
+				t.Fatalf("rotLeft(%v, %v) == '%v', wanted '%v'", tC.a, tC.d, result, tC.want)
+			}
+		})
+	}
+}
