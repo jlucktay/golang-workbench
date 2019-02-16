6a91f960805f5c8c16eb5dc311a9c0a317e55bb5
James Lucktaylor <jlucktay+github@gmail.com>
2019-02-16 07:26:48 +0000

    Expand test coverage (https://www.restapitutorial.com/lessons/httpmethods.html)

--- form3/pilot/path_test.go
+++ form3/pilot/path_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestCreate(t *testing.T) {
 	testCases := []struct {
 		desc     string
 		path     string
@@ -15,31 +15,153 @@ func Test(t *testing.T) {
 			desc:     "Create a new payment",
 			path:     "/payments",
 			verb:     "POST",
-			expected: 0,
+			expected: 201,
+			/*
+				## Notes
+
+				- If your API uses POST to create a resource, be sure to include a Location header in the response that includes the URL of the newly-created resource, along with a 201 status code — that is part of the HTTP standard.
+			*/
 		},
 		{
-			desc:     "Read a single existing payment",
+			desc:     "Create a new payment on a pre-existing ID",
 			path:     "/payments/1234-5678-abcd",
+			verb:     "POST",
+			expected: 409,
+		},
+		{
+			desc:     "Create a new payment on a non-existent valid ID",
+			path:     "/payments/1234-5678-abcd",
+			verb:     "POST",
+			expected: 404,
+		},
+		{
+			desc:     "Create a new payment on an invalid ID",
+			path:     "/payments/my-payment-id",
+			verb:     "POST",
+			expected: 404,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
+
+func TestRead(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		path     string
+		verb     string
+		expected int
+	}{
+		{
+			desc:     "Read the entire collection of existing payments",
+			path:     "/payments",
 			verb:     "GET",
-			expected: 0,
+			expected: 200,
 		},
 		{
 			desc:     "Read a limited collection of existing payments",
 			path:     "/payments?offset=2&limit=2",
 			verb:     "GET",
-			expected: 0,
+			expected: 200,
+		},
+		{
+			desc:     "Read a single existing payment",
+			path:     "/payments/1234-5678-abcd",
+			verb:     "GET",
+			expected: 200,
+		},
+		{
+			desc:     "Read a non-existent payment at a valid ID",
+			path:     "/payments/1234-5678-abcd",
+			verb:     "GET",
+			expected: 404,
+		},
+		{
+			desc:     "Read a non-existent payment at an invalid ID",
+			path:     "/payments/my-payment-id",
+			verb:     "GET",
+			expected: 404,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		path     string
+		verb     string
+		expected int
+	}{
+		{
+			desc:     "Update all existing payments",
+			path:     "/payments",
+			verb:     "PUT",
+			expected: 405,
 		},
 		{
 			desc:     "Update an existing payment",
 			path:     "/payments/1234-5678-abcd",
 			verb:     "PUT",
-			expected: 0,
+			expected: 200,
 		},
 		{
-			desc:     "Delete an existing payment",
+			desc:     "Update a non-existent payment at a valid ID",
+			path:     "/payments/1234-5678-abcd",
+			verb:     "PUT",
+			expected: 404,
+		},
+		{
+			desc:     "Update a non-existent payment at an invalid ID",
+			path:     "/payments/1234-5678-abcd",
+			verb:     "PUT",
+			expected: 404,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		path     string
+		verb     string
+		expected int
+	}{
+		{
+			desc:     "Delete all existing payments",
+			path:     "/payments",
+			verb:     "DELETE",
+			expected: 405,
+		},
+		{
+			desc:     "Delete an existing payment at a valid ID",
 			path:     "/payments/1234-5678-abcd",
 			verb:     "DELETE",
-			expected: 0,
+			expected: 200,
+		},
+		{
+			desc:     "Delete a non-existent payment at a valid ID",
+			path:     "/payments/1234-5678-abcd",
+			verb:     "DELETE",
+			expected: 404,
+		},
+		{
+			desc:     "Delete a non-existent payment at an invalid ID",
+			path:     "/payments/my-payment-id",
+			verb:     "DELETE",
+			expected: 404,
 		},
 	}
 	for _, tC := range testCases {
