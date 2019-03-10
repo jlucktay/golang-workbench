b0e6e49655746fd4c58f449d8a36c67cf92824d1
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-10 07:23:45 +0000

    Download books for local use

--- interfaces/pp2a-asg2/util_test.go
+++ interfaces/pp2a-asg2/util_test.go
@@ -13,13 +13,13 @@ const (
 	dictionary = "dictionary.txt"
 
 	// A Tale of Two Cities, by Charles Dickens
-	book1url = "https://www.gutenberg.org/files/98/98-0.txt"
+	book1 = "98-0.txt"
 
 	// Pride and Prejudice, by Jane Austen
-	book2url = "http://www.gutenberg.org/files/1342/1342-0.txt"
+	book2 = "1342-0.txt"
 
 	// Frankenstein, by Mary Wollstonecraft (Godwin) Shelley
-	book3url = "https://www.gutenberg.org/files/84/84-0.txt"
+	book3 = "84-0.txt"
 )
 
 // mustOpen helps with inlining benchmark functions
