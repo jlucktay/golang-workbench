05d82dcacf63fa847f77723fa4575d36fd1b40af
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-11 20:07:09 +0000

    Implement remaining funcs for OrdLinkedList

--- interfaces/pp2a-asg2/ord_linked_list.go
+++ interfaces/pp2a-asg2/ord_linked_list.go
@@ -1,6 +1,9 @@
 package collection
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // OrdLinkedList is an ordered linked list with linear insert and search
 type OrdLinkedList struct {
@@ -32,21 +35,61 @@ func (o *OrdLinkedList) FreeCollection() {
 // AddCollection adds the string given by the parameter to the WordCollection.
 // The string is added so that the WordCollection is in alphabetical order.
 func (o *OrdLinkedList) AddCollection(word string) int {
-	return FAILURE
+	newNode := &ordLinkedListNode{}
+	current := o.head
+	var previous *ordLinkedListNode
+
+	for current != nil && current.word < word {
+		previous = current
+		current = current.next
+	}
+
+	newNode.word = word
+	newNode.next = current
+	o.size++
+
+	if previous == nil {
+		o.head = newNode
+	} else {
+		previous.next = newNode
+	}
+
+	return SUCCESS
 }
 
 // SearchCollection searches for the string given by the parameter in the
 // WordCollection. This utilises a linear search algorithm, and returns SUCCESS
 // or FAILURE depending upon the outcome of the search.
-func (o *OrdLinkedList) SearchCollection(word string) int {
+func (o *OrdLinkedList) SearchCollection(needle string) int {
+	current := o.head
+
+	for current != nil {
+		if current.word == needle {
+			return SUCCESS
+		} else if current.word > needle {
+			return FAILURE
+		} else {
+			current = current.next
+		}
+	}
+
 	return FAILURE
 }
 
 // SizeCollection returns the number of words in the WordCollection.
 func (o *OrdLinkedList) SizeCollection() int {
-	return FAILURE
+	return o.size
 }
 
 // DisplayCollection prints the contents of the WordCollection to the Writer
 // given by the parameter.
-func (o *OrdLinkedList) DisplayCollection(w io.Writer) {}
+func (o *OrdLinkedList) DisplayCollection(w io.Writer) {
+	i := 0
+	current := o.head
+
+	for current != nil {
+		fmt.Fprintf(w, "Element %d:\t%s\n", i, current.word)
+		i++
+		current = current.next
+	}
+}
