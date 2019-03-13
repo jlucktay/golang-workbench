f9eb8d07d83fda58540916db2bfe3e91ada09f67
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-13 18:09:11 +0000

    Tune up test path UUIDs

--- form3/pilot4/status_test.go
+++ form3/pilot4/status_test.go
@@ -24,19 +24,19 @@ func TestStatusCode(t *testing.T) {
 		},
 		{
 			desc:     "Create a new payment on a pre-existing ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/b2e3ccaa-ac37-45e0-b889-1e6acadf31c8",
 			verb:     http.MethodPost,
 			expected: http.StatusConflict,
 		},
 		{
 			desc:     "Create a new payment on a non-existent valid ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/60c4feb1-bf67-488a-8d04-627bac487c05",
 			verb:     http.MethodPost,
 			expected: http.StatusNotFound,
 		},
 		{
 			desc:     "Create a new payment on an invalid ID",
-			path:     "/payments/my-payment-id",
+			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodPost,
 			expected: http.StatusNotFound,
 		},
@@ -60,13 +60,13 @@ func TestStatusCode(t *testing.T) {
 		},
 		{
 			desc:     "Read a non-existent payment at a valid ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/29e1c453-8cc7-47b8-9c48-7e44b4f9ba26",
 			verb:     http.MethodGet,
 			expected: http.StatusNotFound,
 		},
 		{
 			desc:     "Read a non-existent payment at an invalid ID",
-			path:     "/payments/my-payment-id",
+			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodGet,
 			expected: http.StatusNotFound,
 		},
@@ -84,13 +84,13 @@ func TestStatusCode(t *testing.T) {
 		},
 		{
 			desc:     "Update a non-existent payment at a valid ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/ac5f6fcd-8e69-4949-ad93-d15c51991802",
 			verb:     http.MethodPut,
 			expected: http.StatusNotFound,
 		},
 		{
 			desc:     "Update a non-existent payment at an invalid ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodPut,
 			expected: http.StatusNotFound,
 		},
@@ -102,19 +102,19 @@ func TestStatusCode(t *testing.T) {
 		},
 		{
 			desc:     "Delete an existing payment at a valid ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/a300eb47-efe0-44b0-b729-bed75123bf3a",
 			verb:     http.MethodDelete,
 			expected: http.StatusOK,
 		},
 		{
 			desc:     "Delete a non-existent payment at a valid ID",
-			path:     "/payments/1234-5678-abcd",
+			path:     "/payments/943c4811-f66a-4cb1-8d5b-3ed7da0ce934",
 			verb:     http.MethodDelete,
 			expected: http.StatusNotFound,
 		},
 		{
 			desc:     "Delete a non-existent payment at an invalid ID",
-			path:     "/payments/my-payment-id",
+			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodDelete,
 			expected: http.StatusNotFound,
 		},
@@ -125,12 +125,12 @@ func TestStatusCode(t *testing.T) {
 	// Act & Assert
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			is := is.New(t)
+			i := is.New(t)
 			req, err := http.NewRequest(tC.verb, tC.path, nil)
-			is.NoErr(err)
+			i.NoErr(err)
 			w := httptest.NewRecorder()
 			srv.router.ServeHTTP(w, req)
-			is.Equal(tC.expected, w.Result().StatusCode)
+			i.Equal(tC.expected, w.Result().StatusCode)
 		})
 	}
 }
