3aad45fe439fffc646ae8e876193092bf0ea22ff
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-14 11:05:22 +0000

    Spaces -> tabs

--- form3/pilot4/handlers.read.go
+++ form3/pilot4/handlers.read.go
@@ -45,35 +45,35 @@ func (a *apiServer) adminOnly(h http.HandlerFunc) http.HandlerFunc {
 /*
 // Handler-specific type(s)
 func (s *server) handleSomething() http.HandlerFunc {
-    type request struct {
-        Name string
-    }
-    type response struct {
-        Greeting string `json:"greeting"`
-    }
-    return func(w http.ResponseWriter, r *http.Request) {
-        ...
-    }
+	type request struct {
+		Name string
+	}
+	type response struct {
+		Greeting string `json:"greeting"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		...
+	}
 }
 */
 
 /*
 // sync.Once to setup dependencies
 func (s *server) handleTemplate(files string...) http.HandlerFunc {
-    var (
-        init sync.Once
-        tpl  *template.Template
-        err  error
-    )
-    return func(w http.ResponseWriter, r *http.Request) {
-        init.Do(func(){
-            tpl, err = template.ParseFiles(files...)
-        })
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        // use tpl
-    }
+	var (
+		init sync.Once
+		tpl  *template.Template
+		err  error
+	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		init.Do(func(){
+			tpl, err = template.ParseFiles(files...)
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// use tpl
+	}
 }
 */
