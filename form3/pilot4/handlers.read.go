ab563c6b97760098fedf3a9d8a4953da28ef359e
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-15 09:40:03 +0000

    Refactor around placeholder logic

--- form3/pilot4/handlers.read.go
+++ form3/pilot4/handlers.read.go
@@ -15,15 +15,17 @@ func (a *apiServer) readPayments() httprouter.Handle {
 
 func (a *apiServer) readPaymentById() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		id := uuid.FromStringOrNil(p.ByName("id"))
 
-		if uuid.FromStringOrNil(p.ByName("id")) == uuid.Nil {
+		if id == uuid.Nil {
 			http.Error(w, "Invalid ID.", http.StatusNotFound) // 404
 			return
 		}
 
-		if p.ByName("id") == "29e1c453-8cc7-47b8-9c48-7e44b4f9ba26" {
-			// Placeholder for valid route in the logic
-			http.Error(w, "A payment with this ID does not exist.", http.StatusTeapot) // -> .StatusNotFound 404
+		// Placeholder for valid route in the logic:
+		// -> Read a non-existent payment at a valid ID
+		if id.String() == "29e1c453-8cc7-47b8-9c48-7e44b4f9ba26" {
+			http.Error(w, (&NotFoundError{id}).Error(), http.StatusTeapot) // -> .StatusNotFound 404
 		}
 
 		w.WriteHeader(http.StatusNotImplemented) // 501
