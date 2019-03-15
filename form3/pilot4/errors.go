a1454ce3b34e7e1696a07f027ee1de01013a268b
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-15 16:51:04 +0000

    Add another custom error

--- form3/pilot4/errors.go
+++ form3/pilot4/errors.go
@@ -6,6 +6,14 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+type AlreadyExistsError struct {
+	id uuid.UUID
+}
+
+func (re *AlreadyExistsError) Error() string {
+	return fmt.Sprintf("Payment ID '%s' already exists.", re.id)
+}
+
 type NotFoundError struct {
 	id uuid.UUID
 }
--- form3/pilot4/handlers.create.go
+++ form3/pilot4/handlers.create.go
@@ -15,14 +15,17 @@ func (a *apiServer) createPayments() httprouter.Handle {
 
 func (a *apiServer) createPaymentById() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		if uuid.FromStringOrNil(p.ByName("id")) == uuid.Nil {
+		id := uuid.FromStringOrNil(p.ByName("id"))
+
+		if id == uuid.Nil {
 			http.Error(w, "Invalid ID.", http.StatusNotFound) // 404
 			return
 		}
 
-		// Placeholder for valid route in the logic
-		if p.ByName("id") == "b2e3ccaa-ac37-45e0-b889-1e6acadf31c8" {
-			http.Error(w, "A payment with this ID already exists.", http.StatusTeapot) // -> .StatusConflict) // 409
+		// Placeholder for valid route in the logic:
+		// -> Create a new payment on a pre-existing ID
+		if id.String() == "b2e3ccaa-ac37-45e0-b889-1e6acadf31c8" {
+			http.Error(w, (&AlreadyExistsError{id}).Error(), http.StatusTeapot) // -> .StatusConflict 409
 		}
 
 		http.Error(w, `Cannot specify an ID for payment creation.
