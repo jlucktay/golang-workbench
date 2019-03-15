fa0cbec932a277fdfcf1c1b94f8787cbc97c2513
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-15 17:24:01 +0000

    Tidy up test, and make use of new custom error

--- form3/pilot4/storage_test.go
+++ form3/pilot4/storage_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -23,36 +22,28 @@ func TestStorage(t *testing.T) {
 		t.Run(tC.desc, func(t *testing.T) {
 			t.Logf("Current implementation based on: %s", reflect.TypeOf(tC.ps))
 			i := is.New(t)
-
 			i.NoErr(tC.ps.Init())
-
-			dummyPay := &Payment{
-				Amount: decimal.New(12345, 1),
-			}
+			dummyPay := Payment{Amount: decimal.New(12345, 1)}
 
 			// C
-			newId, errCreate := tC.ps.Create(*dummyPay)
+			newId, errCreate := tC.ps.Create(dummyPay)
 			i.NoErr(errCreate)
 
 			// R
 			readPay, errRead := tC.ps.Read(newId)
 			i.NoErr(errRead)
-
-			i.True(reflect.DeepEqual(dummyPay, &readPay))
+			i.True(reflect.DeepEqual(dummyPay, readPay))
 
 			// U
 			dummyPay.Reference = "ref"
-			i.NoErr(tC.ps.Update(newId, *dummyPay))
+			i.NoErr(tC.ps.Update(newId, dummyPay))
 			updatedPay, _ := tC.ps.Read(newId)
-			i.True(reflect.DeepEqual(dummyPay, &updatedPay))
+			i.True(reflect.DeepEqual(dummyPay, updatedPay))
 
 			// D
 			i.NoErr(tC.ps.Delete(newId))
 			_, errDeleted := tC.ps.Read(newId)
-			i.Equal(
-				errDeleted.Error(),
-				fmt.Sprintf("Payment ID '%s' not found.", newId),
-			)
+			i.Equal(errDeleted, &NotFoundError{newId})
 		})
 	}
 }
