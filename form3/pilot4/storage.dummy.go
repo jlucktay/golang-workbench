bb1be7e5f72f5f6f3a3d88795e329e54520c57ed
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-16 08:38:55 +0000

    Rename in-memory storage appropriately

--- form3/pilot4/storage.dummy.go
+++ form3/pilot4/storage.dummy.go
@@ -4,16 +4,16 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-type dummyStorage struct {
+type inMemoryStorage struct {
 	store map[uuid.UUID]Payment
 }
 
-func (ds *dummyStorage) Init() error {
+func (ds *inMemoryStorage) Init() error {
 	ds.store = make(map[uuid.UUID]Payment)
 	return nil
 }
 
-func (ds *dummyStorage) Create(p Payment) (uuid.UUID, error) {
+func (ds *inMemoryStorage) Create(p Payment) (uuid.UUID, error) {
 	newId, errNew := uuid.NewV4()
 	if errNew != nil {
 		return uuid.Nil, errNew
@@ -22,14 +22,14 @@ func (ds *dummyStorage) Create(p Payment) (uuid.UUID, error) {
 	return newId, nil
 }
 
-func (ds *dummyStorage) Read(id uuid.UUID) (Payment, error) {
+func (ds *inMemoryStorage) Read(id uuid.UUID) (Payment, error) {
 	if p, exists := ds.store[id]; exists {
 		return p, nil
 	}
 	return Payment{}, &NotFoundError{id}
 }
 
-func (ds *dummyStorage) Update(id uuid.UUID, p Payment) error {
+func (ds *inMemoryStorage) Update(id uuid.UUID, p Payment) error {
 	if _, exists := ds.store[id]; exists {
 		ds.store[id] = p
 		return nil
@@ -37,7 +37,7 @@ func (ds *dummyStorage) Update(id uuid.UUID, p Payment) error {
 	return &NotFoundError{id}
 }
 
-func (ds *dummyStorage) Delete(id uuid.UUID) error {
+func (ds *inMemoryStorage) Delete(id uuid.UUID) error {
 	if _, exists := ds.store[id]; exists {
 		delete(ds.store, id)
 		return nil
--- form3/pilot4/storage_test.go
+++ form3/pilot4/storage_test.go
@@ -14,8 +14,8 @@ func TestStorage(t *testing.T) {
 		ps   PaymentStorage
 	}{
 		{
-			desc: "Dummy storage in a map; won't persist across app restarts",
-			ps:   &dummyStorage{},
+			desc: "In-memory storage (map); won't persist across app restarts",
+			ps:   &inMemoryStorage{},
 		},
 	}
 	for _, tC := range testCases {
@@ -23,22 +23,22 @@ func TestStorage(t *testing.T) {
 			t.Logf("Current implementation based on: %s", reflect.TypeOf(tC.ps))
 			i := is.New(t)
 			i.NoErr(tC.ps.Init())
-			dummyPay := Payment{Amount: decimal.New(12345, 1)}
+			testPayment := Payment{Amount: decimal.NewFromFloat(123.45)}
 
 			// C
-			newId, errCreate := tC.ps.Create(dummyPay)
+			newId, errCreate := tC.ps.Create(testPayment)
 			i.NoErr(errCreate)
 
 			// R
 			readPay, errRead := tC.ps.Read(newId)
 			i.NoErr(errRead)
-			i.True(reflect.DeepEqual(dummyPay, readPay))
+			i.True(reflect.DeepEqual(testPayment, readPay))
 
 			// U
-			dummyPay.Reference = "ref"
-			i.NoErr(tC.ps.Update(newId, dummyPay))
+			testPayment.Reference = "ref"
+			i.NoErr(tC.ps.Update(newId, testPayment))
 			updatedPay, _ := tC.ps.Read(newId)
-			i.True(reflect.DeepEqual(dummyPay, updatedPay))
+			i.True(reflect.DeepEqual(testPayment, updatedPay))
 
 			// D
 			i.NoErr(tC.ps.Delete(newId))
