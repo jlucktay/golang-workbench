972fcae6b8b949a08104f8f13702b2a41827e40f
James Lucktaylor <jlucktay+github@gmail.com>
2019-04-18 08:45:47 +0100

    Lint: errcheck

--- monzo/http.go
+++ monzo/http.go
@@ -9,31 +9,32 @@ import (
 )
 
 func getResponse(get url.URL) (io.Reader, error) {
-	req, reqErr := http.NewRequest("GET", get.String(), nil)
-	if reqErr != nil {
-		Error.Printf("URL '%s': request error: %v\n", get.String(), reqErr)
-		return nil, reqErr
+	req, errReq := http.NewRequest("GET", get.String(), nil)
+	if errReq != nil {
+		Error.Printf("URL '%s': request error: %v\n", get.String(), errReq)
+		return nil, errReq
 	}
 
 	req.Header.Add("User-Agent", "jlucktay (monzo-crawler)")
-	res, resErr := http.DefaultClient.Do(req)
+	res, errDo := http.DefaultClient.Do(req)
 
 	// HTTP response errors and non-200 status codes will
 	// 1) log to an error file, and
 	// 2) return a nil buffer and the error from http.DefaultClient
-	if resErr != nil {
-		Error.Printf("URL '%s': response error: %v\n", get.String(), resErr)
-		return nil, resErr
+	if errDo != nil {
+		Error.Printf("URL '%s': response error: %v\n", get.String(), errDo)
+		return nil, errDo
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("URL '%s': status code error: [%d] %s",
-			get.String(), res.StatusCode, res.Status)
+		return nil, fmt.Errorf("URL '%s': status code error: [%d] %s", get.String(), res.StatusCode, res.Status)
 	}
 
 	buf := new(bytes.Buffer)
-	buf.ReadFrom(res.Body)
+	if _, errRead := buf.ReadFrom(res.Body); errRead != nil {
+		return nil, errRead
+	}
 
 	return buf, nil
 }
