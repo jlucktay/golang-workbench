f5e3c53f52da5a2ee536c7880d3afa7d088a105c
James Lucktaylor <jlucktay+github@gmail.com>
2020-12-06 11:16:15 +0000

    feat(graphql/github): pretty print results to terminal

--- graphql/github/go.mod
+++ graphql/github/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
 	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
 	golang.org/x/oauth2 v0.0.0-20201203001011-0b49973bad19
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 )
--- graphql/github/main.go
+++ graphql/github/main.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
+	"golang.org/x/term"
 )
 
 const (
@@ -49,13 +49,46 @@ func main() {
 
 		hasNextPage, endCursor := process(query, &ownedRepos)
 		if !hasNextPage {
+			fmt.Println()
 			break
 		}
 
 		queryVariables["endCursor"] = githubv4.String(endCursor)
 	}
 
-	fmt.Printf("\n%d owned repo(s):\n%s\n", len(ownedRepos), strings.Join(ownedRepos, "\n"))
+	prettyPrint(ownedRepos)
+}
+
+func prettyPrint(input []string) {
+	fmt.Printf("%d owned repo(s):\n", len(input))
+
+	// get terminal width
+	tw, _, errTGS := term.GetSize(int(os.Stdout.Fd()))
+	if errTGS != nil {
+		fmt.Fprintf(os.Stderr, "couldn't get terminal size: %v\n", errTGS)
+		return
+	}
+
+	// get longest repo name
+	longestRepoName := 0
+	for i := range input {
+		if len(input[i]) > longestRepoName {
+			longestRepoName = len(input[i])
+		}
+	}
+
+	// do math to divide lines evenly across width
+	longestRepoName++ // add a single padding space
+	reposPerLine := tw / longestRepoName
+
+	// space out repo names in columns and pretty print
+	for i := 0; i < len(input); i += reposPerLine {
+		for j := 0; j < reposPerLine && i+j < len(input); j++ {
+			fmt.Printf("%-[1]*[2]s", longestRepoName, input[i+j])
+		}
+
+		fmt.Println()
+	}
 }
 
 type queryOwnedRepos *struct {
