8c4d06f19af01bd2ae574b070cd97b76394ba7a4
James Lucktaylor <jlucktay+github@gmail.com>
2020-12-21 18:30:13 +0000

    test(arrowverse): hard-code some expected numbers to test against

--- arrowverse/count_test.go
+++ arrowverse/count_test.go
@@ -8,10 +8,47 @@ import (
 	arrowverse "go.jlucktay.dev/golang-workbench/arrowverse"
 )
 
-func TestGetEpisodeListURLs(t *testing.T) {
+func TestEpisodeNumbers(t *testing.T) {
 	is := is.New(t)
 
-	episodeListURLs, errPE := arrowverse.GetEpisodeListURLs()
-	is.NoErr(errPE)
-	is.True(len(episodeListURLs) >= 11)
+	// Store show/season/episode details for seasons that have finished airing completely.
+	showSeasonEpisodeNumbers := map[string]map[int]int{
+		"Arrow":                     {1: 23, 2: 23, 3: 23, 4: 23, 5: 23, 6: 23, 7: 22, 8: 10},
+		"Batwoman":                  {1: 20},
+		"Birds of Prey":             {1: 13},
+		"Black Lightning":           {1: 13, 2: 16, 3: 16},
+		"Constantine":               {1: 13},
+		"DC's Legends of Tomorrow":  {1: 16, 2: 17, 3: 18, 4: 16, 5: 15},
+		"Freedom Fighters: The Ray": {1: 6, 2: 6},
+		"Supergirl":                 {1: 20, 2: 22, 3: 23, 4: 22, 5: 19},
+		"The Flash (CBS)":           {1: 22},
+		"The Flash (The CW)":        {1: 23, 2: 23, 3: 23, 4: 23, 5: 22, 6: 19},
+		"Vixen":                     {1: 6, 2: 6},
+	}
+
+	episodeListURLs, errGELU := arrowverse.GetEpisodeListURLs()
+	is.NoErr(errGELU)
+	is.Equal(len(episodeListURLs), len(showSeasonEpisodeNumbers))
+
+	for s, elu := range episodeListURLs {
+		// Pin! ref: https://github.com/golang/go/wiki/CommonMistakes#using-reference-to-loop-iterator-variable
+		s, elu := s, elu
+
+		t.Run(s, func(t *testing.T) {
+			// Don't use .Parallel() without pinning
+			t.Parallel()
+
+			show, errGE := arrowverse.GetEpisodes(s, elu)
+			is.NoErr(errGE)
+
+			seasonNumbers, haveShowNumbers := showSeasonEpisodeNumbers[s]
+			is.True(haveShowNumbers)
+
+			for i := 0; i < len(show.Seasons); i++ {
+				if episodeCount, haveSeasonNumbers := seasonNumbers[i+1]; haveSeasonNumbers {
+					is.Equal(len(show.Seasons[i].Episodes), episodeCount)
+				}
+			}
+		})
+	}
 }
