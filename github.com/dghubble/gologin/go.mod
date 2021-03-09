2f93e4ebe4b905f12d623d80912f4af5f74a3b62
James Lucktaylor <jlucktay+github@gmail.com>
2021-03-09 10:45:42 +0000

    feat(dghubble/gologin): set up Google OAuth example

---
+++ github.com/dghubble/gologin/go.mod
@@ -0,0 +1,10 @@
+module go.jlucktay.dev/golang-workbench/github.com/dghubble/gologin
+
+go 1.15
+
+require (
+	github.com/dghubble/gologin/v2 v2.3.0
+	github.com/dghubble/sessions v0.1.0
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
+)
---
+++ github.com/dghubble/gologin/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dghubble/gologin/v2"
+	"github.com/dghubble/gologin/v2/google"
+	"github.com/dghubble/sessions"
+	"golang.org/x/oauth2"
+	googleOAuth2 "golang.org/x/oauth2/google"
+)
+
+const (
+	sessionName     = "example-google-app"
+	sessionSecret   = "example cookie signing secret"
+	sessionUserKey  = "googleID"
+	sessionUsername = "googleName"
+)
+
+// sessionStore encodes and decodes session data stored in signed cookies
+var sessionStore = sessions.NewCookieStore([]byte(sessionSecret), nil)
+
+// Config configures the main ServeMux.
+type Config struct {
+	ClientID     string
+	ClientSecret string
+}
+
+// New returns a new ServeMux with app routes.
+func New(config *Config) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", profileHandler)
+	mux.HandleFunc("/logout", logoutHandler)
+	// 1. Register Login and Callback handlers
+	oauth2Config := &oauth2.Config{
+		ClientID:     config.ClientID,
+		ClientSecret: config.ClientSecret,
+		RedirectURL:  "http://localhost:8080/google/callback",
+		Endpoint:     googleOAuth2.Endpoint,
+		Scopes:       []string{"profile", "email"},
+	}
+	// state param cookies require HTTPS by default; disable for localhost development
+	stateConfig := gologin.DebugOnlyCookieConfig
+	mux.Handle("/google/login", google.StateHandler(stateConfig, google.LoginHandler(oauth2Config, nil)))
+	mux.Handle("/google/callback", google.StateHandler(stateConfig, google.CallbackHandler(oauth2Config, issueSession(), nil)))
+	return mux
+}
+
+// issueSession issues a cookie session after successful Google login
+func issueSession() http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		googleUser, err := google.UserFromContext(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// 2. Implement a success handler to issue some form of session
+		session := sessionStore.New(sessionName)
+		session.Values[sessionUserKey] = googleUser.Id
+		session.Values[sessionUsername] = googleUser.Name
+		if err := session.Save(w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, req, "/profile", http.StatusFound)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// profileHandler shows a personal profile or a login button (unauthenticated).
+func profileHandler(w http.ResponseWriter, req *http.Request) {
+	session, err := sessionStore.Get(req, sessionName)
+	if err != nil {
+		// welcome with login button
+		page, _ := ioutil.ReadFile("home.html")
+		fmt.Fprint(w, string(page))
+		return
+	}
+	// authenticated profile
+	fmt.Fprintf(w, `<p>You are logged in %s!</p><form action="/logout" method="post"><input type="submit" value="Logout"></form>`, session.Values[sessionUsername])
+}
+
+// logoutHandler destroys the session on POSTs and redirects to home.
+func logoutHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		sessionStore.Destroy(w, sessionName)
+	}
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+// main creates and starts a Server listening.
+func main() {
+	const address = "localhost:8080"
+	// read credentials from environment variables if available
+	config := &Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+	}
+	// allow consumer credential flags to override config fields
+	clientID := flag.String("client-id", "", "Google Client ID")
+	clientSecret := flag.String("client-secret", "", "Google Client Secret")
+	flag.Parse()
+	if *clientID != "" {
+		config.ClientID = *clientID
+	}
+	if *clientSecret != "" {
+		config.ClientSecret = *clientSecret
+	}
+	if config.ClientID == "" {
+		log.Fatal("Missing Google Client ID")
+	}
+	if config.ClientSecret == "" {
+		log.Fatal("Missing Google Client Secret")
+	}
+
+	log.Printf("Starting Server listening on %s\n", address)
+	err := http.ListenAndServe(address, New(config))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
