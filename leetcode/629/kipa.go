19d808463679ea5f117ef9a79862aa954b142046
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-17 13:31:16 +0100

    refactor(leetcode/629): try Approach 4 implementation from solutions

--- leetcode/629/kipa.go
+++ leetcode/629/kipa.go
@@ -2,26 +2,30 @@ package kipa
 
 func KInversePairs(n int, k int) int {
 	dp := [1001][1001]int{}
+	const M = 1000000007
 
 	for i := 1; i <= n; i++ {
 		for j := 0; j <= k; j++ {
 			if j == 0 {
 				dp[i][j] = 1
 			} else {
-				for p := 0; p <= min(j, i-1); p++ {
-					dp[i][j] = (dp[i][j] + dp[i-1][j-p]) % 1000000007
+				// (j - i) >= 0 ? dp[i - 1][j - i] : 0
+				x := 0
+				if j-i >= 0 {
+					x = dp[i-1][j-i]
 				}
+
+				val := (dp[i-1][j] + M - x) % M
+				dp[i][j] = (dp[i][j-1] + val) % M
 			}
 		}
 	}
 
-	return dp[n][k]
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
+	// k > 0 ? dp[n][k - 1] : 0
+	a := 0
+	if k > 0 {
+		a = dp[n][k-1]
 	}
 
-	return b
+	return (dp[n][k] + M - a) % M
 }
