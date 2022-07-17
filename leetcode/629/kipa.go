25dcf7bcf0c4994e00ffa329a8eac1bf56df22de
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-17 12:36:46 +0100

    refactor(leetcode/629): speed up to beat TLE

--- leetcode/629/kipa.go
+++ leetcode/629/kipa.go
@@ -1,23 +1,21 @@
 package kipa
 
-var v = [1001][1001]int{}
-
 func KInversePairs(n int, k int) int {
-	if n == 0 || k < 0 {
-		return 0
-	}
+	dp := [1001][1001]int{}
 
-	if k == 0 {
-		return 1
-	}
-
-	if v[n][k] == 0 {
-		for i := 0; i <= min(n-1, k); i++ {
-			v[n][k] = (v[n][k] + KInversePairs(n-1, k-i)) % 1000000007
+	for i := 1; i <= n; i++ {
+		for j := 0; j <= k; j++ {
+			if j == 0 {
+				dp[i][j] = 1
+			} else {
+				for p := 0; p <= min(i-1, j); p++ {
+					dp[i][j] = (dp[i][j] + dp[i-1][j-p]) % 1000000007
+				}
+			}
 		}
 	}
 
-	return v[n][k]
+	return dp[n][k]
 }
 
 func min(a, b int) int {
