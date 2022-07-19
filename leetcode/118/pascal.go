f905aeff56d1599f0261beb12bba79fda9b59a4d
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-19 17:05:50 +0100

    refactor(leetcode/118): optimise

--- leetcode/118/pascal.go
+++ leetcode/118/pascal.go
@@ -6,14 +6,13 @@ func generate(numRows int) [][]int {
 	result[0] = append(result[0], 1)
 
 	for i := 1; i < numRows; i++ {
-		result[i] = make([]int, 0)
-		result[i] = append(result[i], 1)
+		result[i] = make([]int, i+1)
+		result[i][0] = 1
+		result[i][i] = 1
 
 		for j := 1; j < i; j++ {
-			result[i] = append(result[i], result[i-1][j-1]+result[i-1][j])
+			result[i][j] = result[i-1][j-1] + result[i-1][j]
 		}
-
-		result[i] = append(result[i], 1)
 	}
 
 	return result
