1bfaebe1e13b807c0b116467e8fadec642e8f8cb
James Lucktaylor <jlucktay+github@gmail.com>
2022-08-24 17:04:10 +0100

    feat(charm/harmonica): try out an example

---
+++ github.com/charmbracelet/harmonica/go.mod
@@ -0,0 +1,25 @@
+module go.jlucktay.dev/golang-workbench/github.com/charmbracelet/harmonica
+
+go 1.19
+
+require (
+	github.com/charmbracelet/bubbletea v0.22.1
+	github.com/charmbracelet/harmonica v0.2.0
+	github.com/charmbracelet/lipgloss v0.5.0
+)
+
+require (
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.12.0 // indirect
+	github.com/rivo/uniseg v0.3.4 // indirect
+	golang.org/x/sys v0.0.0-20220823224334-20c2bfdbfe24 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ github.com/charmbracelet/harmonica/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/harmonica"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	fps          = 60
+	spriteWidth  = 12
+	spriteHeight = 5
+	frequency    = 7.0
+	damping      = 0.15
+)
+
+var (
+	helpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{Light: "246", Dark: "241"}).
+			MarginTop(1).
+			MarginLeft(2)
+
+	spriteStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#575BD8"))
+)
+
+type frameMsg time.Time
+
+func animate() tea.Cmd {
+	return tea.Tick(time.Second/fps, func(t time.Time) tea.Msg {
+		return frameMsg(t)
+	})
+}
+
+func wait(d time.Duration) tea.Cmd {
+	return func() tea.Msg {
+		time.Sleep(d)
+		return nil
+	}
+}
+
+type model struct {
+	spring harmonica.Spring
+	x      float64
+	xVel   float64
+}
+
+func (model) Init() tea.Cmd {
+	return tea.Sequentially(wait(time.Second/2), animate())
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case tea.KeyMsg:
+		return m, tea.Quit
+
+	// Step foreward one frame
+	case frameMsg:
+		const targetX = 60
+
+		// Update x position (and velocity) with our spring.
+		m.x, m.xVel = m.spring.Update(m.x, m.xVel, targetX)
+
+		// Quit when we're basically at the target position.
+		if math.Abs(m.x-targetX) < 0.01 {
+			return m, tea.Sequentially(wait(time.Second*3/4), tea.Quit)
+		}
+
+		// Request next frame
+		return m, animate()
+
+	default:
+		return m, nil
+	}
+}
+
+func (m model) View() string {
+	var out strings.Builder
+	fmt.Fprint(&out, "\n")
+
+	x := int(math.Round(m.x))
+	if x < 0 {
+		return ""
+	}
+
+	spriteRow := spriteStyle.Render(strings.Repeat("/", spriteWidth))
+	row := strings.Repeat(" ", x) + spriteRow + "\n"
+	fmt.Fprint(&out, strings.Repeat(row, spriteHeight))
+
+	fmt.Fprint(&out, helpStyle.Render("Press any key to quit"))
+
+	return out.String()
+}
+
+func main() {
+	m := model{
+		spring: harmonica.NewSpring(harmonica.FPS(fps), frequency, damping),
+	}
+
+	if err := tea.NewProgram(m).Start(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
