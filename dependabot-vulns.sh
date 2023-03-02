68196d9d17645adf4bc614f0f5e6d4a42718cdfa
James Lucktaylor <jlucktay+github@gmail.com>
2023-03-02 18:01:46 +0000

    feat(dependabot-vulns.sh): adapat to be runnable from other projects

--- dependabot-vulns.sh
+++ dependabot-vulns.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")" &> /dev/null && pwd)"
-cd "$script_dir"
-
 # for each node (vulnerability) in the JSON
 # - join git toplevel to vuln manifest path, minus go.sum
 # - go to that directory
 # - run 'go get -u <sec vuln pkg name>' et al
 # - git commit
 
-top_level=$(git rev-parse --show-toplevel)
+if ! top_level=$(git rev-parse --show-toplevel); then
+  echo >&2 "Not running from inside a git repo."
+  exit 1
+fi
 
-gql_result=$(gh api graphql -f=query='
+current_project=$(basename "$top_level")
+
+gql_result=$(gh api graphql -f=query="
   query{
-    repository(name: "golang-workbench", owner: "jlucktay") {
+    repository(name: \"$current_project\", owner: \"jlucktay\") {
       vulnerabilityAlerts(last: 100, states: OPEN) {
         nodes {
           number
@@ -26,11 +28,14 @@ gql_result=$(gh api graphql -f=query='
             }
             vulnerableVersionRange
           }
+          securityAdvisory {
+            notificationsPermalink
+          }
         }
       }
     }
   }
-')
+")
 
 list_length=$(jq '.data.repository.vulnerabilityAlerts.nodes | length' <<< "$gql_result")
 
@@ -44,10 +49,22 @@ for ((i = 0; i < list_length; i++)); do
   vuln_pkg=$(jq --exit-status --raw-output \
     ".data.repository.vulnerabilityAlerts.nodes[$i].securityVulnerability.package.name" <<< "$gql_result")
 
+  vuln_link=$(jq --exit-status --raw-output \
+    ".data.repository.vulnerabilityAlerts.nodes[$i].securityAdvisory.notificationsPermalink" <<< "$gql_result")
+
+  this_module=$(git rev-parse --show-prefix)
+  this_module=${this_module%/}
+
+  project_directory="$top_level/$this_module"
+
+  if [[ -n $this_module ]]; then
+    this_module="($this_module)"
+  fi
+
   (
     set -x
 
-    cd "$top_level/${vuln_path%/go.sum}"
+    cd "$project_directory"
 
     go get -u -v "$vuln_pkg"
     go mod tidy -go=1.20
@@ -57,7 +74,10 @@ for ((i = 0; i < list_length; i++)); do
     git unstage
     git add -- ./go.{mod,sum}
 
-    if ! git commit --message="build(${vuln_path%/go.sum}): upgrade Go to 1.20" > /dev/null; then
+    if ! git commit \
+      --message="build$this_module: address Dependabot vuln $vuln_number" \
+      --message="URL: $vuln_link" \
+      > /dev/null; then
       echo "Vuln #$vuln_number affecting '$vuln_pkg' in '${vuln_path%/go.sum}' seems to be resolved already."
     fi
   )
