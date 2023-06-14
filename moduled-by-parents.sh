4b41565cd8d625642d7a9bd26a10acf1df4d900f
James Lucktaylor <jlucktay+github@gmail.com>
2023-06-14 10:29:50 +0100

    feat(mbp.sh): run relative to a repo's top level directory

--- moduled-by-parents.sh
+++ moduled-by-parents.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if ! top_level=$(git rev-parse --show-toplevel); then
+  echo >&2 "Not running from inside a git repo."
+  exit 1
+fi
+
 # Find directories containing Go code that is not covered by a module that isn't the repo root.
-fd --extension go --type file --exec bash -c 'echo {//}' \
+fd --extension go --type file --exec bash -c 'echo {//}' "$top_level" \
   | sort -fu \
   | xargs -I % -n 1 bash -c 'if ! stat %/go.mod &> /dev/null; then ( echo %; cd %; go env GOMOD; ); fi' \
-  | rg --before-context=1 --color=never --fixed-strings "$(pwd || true)/go.mod"
+  | rg --before-context=1 --color=never --fixed-strings "$top_level"/go.mod
