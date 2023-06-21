dcbd576a034ccdb9fd92ec4c895b0e5fe4e620e4
James Lucktaylor <jlucktay+github@gmail.com>
2023-06-21 12:50:44 +0100

    feat(mlb/biginns): add flags and more error handling

--- mlb/biginns/go.mod
+++ mlb/biginns/go.mod
@@ -4,11 +4,27 @@ go 1.20
 
 require (
 	github.com/anaskhan96/soup v1.2.5
+	github.com/lmittmann/tint v0.3.4
+	github.com/mattn/go-isatty v0.0.19
+	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.16.0
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- mlb/biginns/main.go
+++ mlb/biginns/main.go
@@ -7,33 +7,86 @@ import (
 	"time"
 
 	"github.com/anaskhan96/soup"
+	"github.com/lmittmann/tint"
+	"github.com/mattn/go-isatty"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"golang.org/x/exp/slog"
 )
 
-func main() {
-	const (
-		biURL = "https://www.mlb.com/live-stream-games/big-inning"
-		tz    = "EST5EDT"
-	)
+const (
+	biURL = "https://www.mlb.com/live-stream-games/big-inning"
+	tz    = "EST5EDT"
+)
 
+func main() {
+	// Initialise config from flags, and bind into Viper.
+	pflag.IntP("earliest", "e", 8, "The earliest hour of the start time to show, in 24h time")
+	pflag.IntP("latest", "l", 20, "The latest hour of the start time to show, in 24h time")
+	pflag.BoolP("debug", "d", false, "Enable debug logging")
+
+	pflag.Parse()
+	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
+		fmt.Fprintf(os.Stderr, "error parsing command line flags: %v", err)
+		os.Exit(1)
+	}
+
+	// Set up logging.
+	logOpts := &tint.Options{
+		Level:      slog.LevelInfo,
+		TimeFormat: time.RFC3339,
+
+		// https://github.com/lmittmann/tint#automatically-enabledisable-colors
+		NoColor: !isatty.IsTerminal(os.Stderr.Fd()),
+	}
+
+	if viper.GetBool("debug") {
+		logOpts.AddSource = true
+		logOpts.Level = slog.LevelDebug
+	}
+
+	slog.SetDefault(slog.New(tint.NewHandler(os.Stderr, logOpts)))
+
+	// Grab the webpage to scrape/parse.
 	resp, err := soup.Get(biURL)
 	if err != nil {
-		slog.Error("could not GET", slog.String("url", biURL), slog.Any("err", err))
+		slog.Error("could not GET", slog.String("url", biURL), tint.Err(err))
 		os.Exit(1)
 	}
 
 	location, err := time.LoadLocation(tz)
 	if err != nil {
-		slog.Error("could not load location", slog.String("tz", tz), slog.Any("err", err))
+		slog.Error("could not load location", slog.String("tz", tz), tint.Err(err))
 		os.Exit(1)
 	}
 
 	doc := soup.HTMLParse(resp)
 
-	things := doc.Find("table", "class", "p-table").Find("tbody").FindAll("tr")
+	table := doc.Find("table", "class", "p-table")
+	if table.Error != nil {
+		slog.Error("can't find table", tint.Err(table.Error))
+		os.Exit(1)
+	}
 
-	for _, thing := range things {
-		ft := thing.FullText()
+	tableBody := table.Find("tbody")
+	if tableBody.Error != nil {
+		slog.Error("can't find body inside table", tint.Err(tableBody.Error))
+		os.Exit(1)
+	}
+
+	tableRows := tableBody.FindAll("tr")
+	if len(tableRows) == 0 {
+		slog.Error("can't find rows in table body")
+		os.Exit(1)
+	}
+
+	for _, tableRow := range tableRows {
+		if tableRow.Error != nil {
+			slog.Error("table row", tint.Err(tableRow.Error))
+			continue
+		}
+
+		ft := tableRow.FullText()
 		xft := strings.Split(ft, "\n\t")
 
 		if len(xft) < 3 {
@@ -44,11 +97,11 @@ func main() {
 
 		startTime, err := time.ParseInLocation("1/2/2006 3:04 PM", parseMe, location)
 		if err != nil {
-			slog.Error("could not parse time from string", slog.String("input", parseMe), slog.Any("err", err))
+			slog.Error("could not parse time from string", slog.String("input", parseMe), tint.Err(err))
 			os.Exit(1)
 		}
 
-		if startTime.Local().Hour() >= 8 && startTime.Local().Hour() <= 20 {
+		if startTime.Local().Hour() >= viper.GetInt("earliest") && startTime.Local().Hour() <= viper.GetInt("latest") {
 			slog.Debug("start time", slog.Time("t", startTime), slog.Time("local", startTime.Local()))
 
 			fmt.Printf("%s\n", startTime.Local().Format(time.RFC850))
