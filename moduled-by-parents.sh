47ff454f5475ca8003cc91331a33d150256a8d22
James Lucktaylor <jlucktay+github@gmail.com>
2023-12-24 07:39:16 +0000

    fix(moduled-by-parents.sh): set base directory correctly, instead of passing to bash exec

--- moduled-by-parents.sh
+++ moduled-by-parents.sh
@@ -6,8 +6,9 @@ if ! top_level=$(git rev-parse --show-toplevel); then
   exit 1
 fi
 
-# Find directories containing Go code that is not covered by a module that isn't the repo root.
-fd --extension go --type file --exec bash -c 'echo {//}' "$top_level" \
+# Find directories containing Go code files which are included in a module, where that module is at the repo root.
+fd --extension go --type file --base-directory "$top_level" --exec bash -c 'echo {//}' \
   | sort -fu \
-  | xargs -I % -n 1 bash -c 'if ! stat %/go.mod &> /dev/null; then ( echo %; cd %; go env GOMOD; ); fi' \
+  | sed "s|^\.|$top_level|" \
+  | xargs -I % -S 1024 -n 1 bash -c 'if ! stat %/go.mod &> /dev/null; then ( echo %; go env -C % GOMOD; ); fi' \
   | rg --before-context=1 --color=never --fixed-strings "$top_level"/go.mod
